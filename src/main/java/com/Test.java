2514f1145645a5c3947318fad7bda80502058372
luoding <1808392621@qq.com>
2017-05-28 00:01:33 +0800

    test

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * author: ding.luo
+ * time: 17-5-28 上午12:00
+ */
+public class Test {
+
+    public static void main(String [] args){
+        System.out.println("just do IT");
+    }
+}
